ab30502c2381d408c4e36a0e18f51aab37c97fa7
dulasimca <dulasiayya.kathamuthu@tesslve.com>
2021-09-03 13:32:28 +0530

    Online Assessment changes done

---
+++ Controllers/Forms/OnlineAssessmentController.cs
@@ -0,0 +1,58 @@
+﻿using EducationPortalAPI.ManageSQL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EducationPortalAPI.Controllers.Forms
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OnlineAssessmentController : ControllerBase
+    {
+         
+        [HttpPost("{id}")]
+        public string Post(OnlineAssessmentMasterEntity entity)
+        {
+            ManageOnlineAssessment onlineAssessment = new ManageOnlineAssessment();           
+            var result = onlineAssessment.InsertData(entity);
+            return JsonConvert.SerializeObject(result);
+        }
+        [HttpGet("{id}")]
+        public string Get(string SchoolID)
+        {
+            ManageSQLConnection manageSQL = new ManageSQLConnection();
+            DataSet ds = new DataSet();
+           
+            return "";
+        }
+
+    }
+    public class OnlineAssessmentMasterEntity
+    {
+        public Int64 RowId { get; set; }
+        public string SchoolId { get; set; }
+        public string TestName { get; set; }
+        public string TestDescription { get; set; }
+        public DateTime AssessmentDate { get; set; }
+        public bool Flag { get; set; }
+        public List<OnlineAssessmentQuestionsEntity> Questions { get; set; }
+    }
+    public class OnlineAssessmentQuestionsEntity
+    {
+        public Int64 QuestionId { get; set; }
+        public Int64 RowId { get; set; }
+        public string QuestionDetails { get; set; }
+        public List<OnlineAssessmentOptions> Options { get;set;}
+    }
+    public class OnlineAssessmentOptions
+    {
+       public Int64  AnswerId { get; set; }
+       public Int64  QuestionId { get; set; }
+       public bool IsAnswer { get; set; }
+    }
+}
---
+++ ManageSQL/ManageOnlineAssessment.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using EducationPortalAPI.Controllers.Forms;
+
+namespace EducationPortalAPI.ManageSQL
+{
+    public class ManageOnlineAssessment
+    {
+        SqlConnection sqlConnection = new SqlConnection();
+        SqlCommand sqlCommand = new SqlCommand();
+
+        SqlDataAdapter dataAdapter;
+ 
+        public bool InsertData(OnlineAssessmentMasterEntity entity)
+        {
+            SqlTransaction objTrans = null;
+            sqlConnection = new SqlConnection(GlobalVariable.ConnectionString);
+            DataSet ds = new DataSet();
+            sqlCommand = new SqlCommand();
+            try
+            {
+                if (sqlConnection.State == 0)
+                {
+                    sqlConnection.Open();
+                }
+                objTrans = sqlConnection.BeginTransaction();
+                sqlCommand.Transaction = objTrans;
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText = "InsertOnlineAssessmentMaster";
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddWithValue("@RowId1", entity.RowId);
+                sqlCommand.Parameters.AddWithValue("@TestName", entity.TestName);
+                sqlCommand.Parameters.AddWithValue("@TestDescription", entity.TestDescription);
+                sqlCommand.Parameters.AddWithValue("@SchoolId", entity.SchoolId);
+                sqlCommand.Parameters.AddWithValue("@RowId", entity.RowId);
+                sqlCommand.Parameters.AddWithValue("@Flag", entity.Flag);
+                sqlCommand.Parameters.AddWithValue("@AssessmentDate", entity.AssessmentDate);
+                sqlCommand.Parameters.Add("@RowId", SqlDbType.BigInt, 13);
+                sqlCommand.Parameters["@RowId"].Direction = ParameterDirection.Output;
+                sqlCommand.ExecuteNonQuery();
+                int AssessmentRowid = (int)sqlCommand.Parameters["@RowId"].Value;
+
+                sqlCommand.Parameters.Clear();
+                sqlCommand.Dispose();
+
+                foreach (var item in entity.Questions)
+                {
+                    sqlCommand.Parameters.Clear();
+                    sqlCommand.Dispose();
+
+                    sqlCommand = new SqlCommand();
+                    sqlCommand.Transaction = objTrans;
+                    sqlCommand.Connection = sqlConnection;
+                    sqlCommand.CommandText = "InsertOnlineAssessmentQuestions";
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.Parameters.AddWithValue("@RowId", AssessmentRowid);
+                    sqlCommand.Parameters.AddWithValue("@QuestionId", item.QuestionId);
+                    sqlCommand.Parameters.AddWithValue("@QuestionDetails", item.QuestionDetails);
+                    sqlCommand.Parameters.Add("@QuestionId", SqlDbType.BigInt, 13);
+                    sqlCommand.Parameters["@QuestionId"].Direction = ParameterDirection.Output;
+                    sqlCommand.ExecuteNonQuery();
+                    int QuestionId= (int)sqlCommand.Parameters["@QuestionId"].Value;
+
+                    foreach (var Option in item.Options)
+                    {
+                        sqlCommand.Parameters.Clear();
+                        sqlCommand.Dispose();
+
+                        sqlCommand = new SqlCommand();
+                        sqlCommand.Transaction = objTrans;
+                        sqlCommand.Connection = sqlConnection;
+                        sqlCommand.CommandText = "InsertOnlineAssessmentOptions";
+                        sqlCommand.CommandType = CommandType.StoredProcedure;
+                        sqlCommand.Parameters.AddWithValue("@AnswerId", Option.AnswerId);
+                        sqlCommand.Parameters.AddWithValue("@QuestionId", QuestionId);
+                        sqlCommand.Parameters.AddWithValue("@IsAnswer", Option.IsAnswer);  
+                        sqlCommand.ExecuteNonQuery();
+                    }
+                }
+                sqlCommand.Parameters.Clear();
+                sqlCommand.Dispose();
+                objTrans.Commit();
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                objTrans.Rollback();
+                Console.WriteLine(ex);
+                return false;
+            }
+            finally
+            {
+                sqlConnection.Close();
+                sqlCommand.Dispose();
+                ds.Dispose();
+                dataAdapter = null;
+            }
+        }
+    }
+}
