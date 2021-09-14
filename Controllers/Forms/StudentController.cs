2c6fa13ee2f8e376038db1d7f90f510b70d761af
Adithya0802 <adithya08feb1997@gmail.com>
2021-09-14 10:57:11 +0530

    studentcontroller added

---
+++ Controllers/Forms/StudentController.cs
@@ -0,0 +1,32 @@
+﻿using EducationPortalAPI.ManageSQL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EducationPortalAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+
+        [HttpGet("{id}")]
+        public string Get(String SchoolId, String ClassId, String SectionId)
+        {
+            ManageSQLConnection manageSQL = new ManageSQLConnection();
+            DataSet ds = new DataSet();
+            List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
+            sqlParameters.Add(new KeyValuePair<string, string>("@SchoolId", SchoolId));
+            sqlParameters.Add(new KeyValuePair<string, string>("@ClassId", ClassId));
+            sqlParameters.Add(new KeyValuePair<string, string>("@SectionId", SectionId));
+            ds = manageSQL.GetDataSetValues("GetStudent", sqlParameters);
+            return JsonConvert.SerializeObject(ds.Tables[0]);
+        }
+    }
+}
+
