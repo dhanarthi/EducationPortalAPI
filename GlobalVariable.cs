80d6b5ca46bcbc739ad152f9c30095e70fbad52f
srikanth2419 <srikanthraj.raj@gmail.com>
2021-09-24 15:52:26 +0530

    Globalvariables UploadContraller

--- GlobalVariable.cs
+++ GlobalVariable.cs
@@ -9,10 +9,11 @@ namespace EducationPortalAPI
     {
         //Testing connection
         public const string ConnectionString = "data source=180.179.49.72;initial catalog=TNEducation;user id = sqladmin; password =sql@svc&ac!72;";
-        
+        public const string FolderPath = "D://Angular Project//Education-portal-live//src//assets//layout//";
 
         //Live connection
         //public const string ConnectionString = "data source=localhost;initial catalog=TNCSCSCM;user id =sqladmin; password =sql@ &ac!34;";
-      
+        //public const string ReportPath = "C://Repos//LiveCode//TNCSCWebSite//assets//";
+        
     }
 }
