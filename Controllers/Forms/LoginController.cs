6be4bf2f2ca9c19692d52d3855cdc9677a216787
dulasimca <dulasiayya.kathamuthu@tesslve.com>
2021-09-29 11:58:45 +0530

    Login changes

---
+++ Controllers/Forms/LoginController.cs
@@ -0,0 +1,75 @@
+﻿using EducationPortalAPI.ManageSQL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EducationPortalAPI.Model;
+
+namespace EducationPortalAPI.Controllers.Forms
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public Tuple<bool, string, string> Get(int Type, string Value, int RoleId, string Password)
+        {
+            Security security = new Security();
+            string RoleName = string.Empty;
+            try
+            {
+                RoleName = RoleId == 6 ? "Student" : RoleId == 5 ? "Teacher" : "Admin";
+                ManageSQLConnection manageSQLConnection = new ManageSQLConnection();
+
+                //Check the document Approval
+                List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
+                sqlParameters.Add(new KeyValuePair<string, string>("@Type",Convert.ToString(Type)));
+                sqlParameters.Add(new KeyValuePair<string, string>("@Id", Value)); //2
+                var data = manageSQLConnection.GetDataSetValues("GetRegistration", sqlParameters);
+                if (Type == 2) // check the 
+                {
+                    if(data.Tables.Count>0)
+                    {
+                        if(data.Tables[0].Rows.Count>0)
+                        {
+                            if(Convert.ToInt32(data.Tables[0].Rows[0]["RoleId"])  == RoleId)
+                            {
+                                if(Convert.ToString(data.Tables[0].Rows[0]["EncrptedPwd"]) == security.Encryptword(Password))
+                                {
+                                    return new Tuple<bool, string, string>(true, "Login Successfully ", JsonConvert.SerializeObject(data.Tables[0]));
+
+                                }
+                                else
+                                {
+                                    return new Tuple<bool, string, string>(false, "Password Incorrect, Pleas enter correct password" , "");
+                                }
+                            }
+                            else
+                            {
+                                return new Tuple<bool, string, string>(false, "You are authorized to login the " + RoleName, "");
+                            }
+                        }
+                        else
+                        {
+                            return new Tuple<bool, string, string>(false, "Invalid User Name", "");
+                        }
+                    }
+                    else
+                    {
+                        return new Tuple<bool, string, string>(false, "Invalid User Name", "");
+                    }
+                }
+                return new Tuple<bool, string, string> (false," Please try later", "");
+            }
+            catch (Exception ex)
+            {
+                AuditLog.WriteError(ex.Message);
+                return new Tuple<bool, string, string>(false, "Please try later", "");
+            }
+
+        }
+    }
+}
