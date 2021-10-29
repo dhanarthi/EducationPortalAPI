768e3f9710f314f8d5b6d0398514142dbee855f9
Aditiya_T661 <Aditiya_T661@TANDVCL0041>
2021-10-29 11:39:34 +0530

    feetype cont and stud list con added

---
+++ Controllers/Masters/StudentListController.cs
@@ -0,0 +1,28 @@
+﻿using EducationPortalAPI.ManageSQL;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EducationPortalAPI.Controllers.Masters
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentListController : Controller
+    {
+        [HttpGet("{id}")]
+        public string Get(int SectionId, int ClassId)
+        {
+            ManageSQLConnection manageSQL = new ManageSQLConnection();
+            DataSet ds = new DataSet();
+            List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
+            sqlParameters.Add(new KeyValuePair<string, string>("@SectionId", Convert.ToString(SectionId)));
+            sqlParameters.Add(new KeyValuePair<string, string>("@ClassId", Convert.ToString(ClassId)));
+            ds = manageSQL.GetDataSetValues("GetStudentList", sqlParameters);
+            return JsonConvert.SerializeObject(ds.Tables[0]);
+        }
+    }
+}
